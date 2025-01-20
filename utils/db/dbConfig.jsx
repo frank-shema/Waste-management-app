1f3fbc1e8f71f7de712219af73dadde084c11fa3
frank-shema <shemafranko0987@gmail.com>
2025-01-20 14:43:44 +0200

    added hooks

--- utils/db/dbConfig.jsx
+++ utils/db/dbConfig.jsx
@@ -1,8 +1,7 @@
-
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 const sql = neon(
-    process.env.DATABASE_URL,
+    "postgresql://waste-management-db_owner:EUMRJC1FBpD9@ep-frosty-thunder-a8fritow.eastus2.azure.neon.tech/waste-management-db?sslmode=require"
 );
 export const db = drizzle(sql, { schema });
